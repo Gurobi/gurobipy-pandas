606b41ac2b42c86ab89139a5077a1749b3bbcbd2
Simon Bowly <bowly@gurobi.com>
2022-05-30 09:57:50 -0400

    Implement variable indexing by columns

--- pdcomfi.py
+++ pdcomfi.py
@@ -8,9 +8,15 @@ class GRBDataFrameAccessor:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
-    def addVars(self, model, name, lb=0.0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS):
+    def addVars(self, model, name, index=None, lb=0.0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS):
         """Name is a required argument as the new column needs a name.
         This makes a signature slightly different to model.addVars"""
-        x = model.addVars(self._obj.index, name=name, lb=lb, ub=ub, vtype=vtype)
-        xs = pd.Series(data=x, index=self._obj.index, name=name)
+        if index is None:
+            indices = self._obj.index
+        elif isinstance(index, str):
+            indices = self._obj[index]
+        else:
+            indices = self._obj[index].itertuples(index=False)
+        x = model.addVars(indices, name=name, lb=lb, ub=ub, vtype=vtype)
+        xs = pd.Series(data=x.values(), index=self._obj.index, name=name)
         return self._obj.join(xs)
