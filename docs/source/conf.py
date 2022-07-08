1fafc9b5e3f0b35c26fc8c9523bdf1499b1b4057
Simon Bowly <bowly@gurobi.com>
2022-07-08 12:39:36 +1000

    [#5] Setup with sphinx-quickstart
    Defaults from sphinx-quickstart, plus a requirements file for extensions.
    Closes #5.

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = 'gurobipy-pandas'
+copyright = '2022, Gurobi Optimization'
+author = 'Gurobi Optimization'
+release = '0.0.1a0'
+version = release
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx_tabs.tabs',
+    'sphinx_toolbox.collapse',
+    'sphinx_toolbox.code',
+]
+
+pygments_style = "vs"
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+
+epub_show_urls = 'footnote'
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. gurobipy-pandas documentation master file, created by
+   sphinx-quickstart on Fri Jul  8 12:28:14 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to gurobipy-pandas's documentation!
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
