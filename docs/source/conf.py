7e1c04dbafcad1dc753d31c3a0f9a69eb850048f
Simon Bowly <bowly@gurobi.com>
2022-07-28 08:34:13 +1000

    Run pre-commit --all-files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,9 +4,9 @@
 
 import gurobipy_pandas
 
-project = 'gurobipy-pandas'
-copyright = '2022, Gurobi Optimization'
-author = 'Gurobi Optimization'
+project = "gurobipy-pandas"
+copyright = "2022, Gurobi Optimization"
+author = "Gurobi Optimization"
 
 version = gurobipy_pandas.__version__
 release = version
@@ -14,38 +14,38 @@ release = version
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx_tabs.tabs',
-    'sphinx_toolbox.collapse',
-    'sphinx_toolbox.code',
-    'nbsphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx_tabs.tabs",
+    "sphinx_toolbox.collapse",
+    "sphinx_toolbox.code",
+    "nbsphinx",
 ]
 
 pygments_style = "vs"
 docutils_tab_width = 4
 nbsphinx_custom_formats = {
-    '.md': ['jupytext.reads', {'fmt': 'myst'}],
+    ".md": ["jupytext.reads", {"fmt": "myst"}],
 }
 
 nbsphinx_kernel_name = "python3"
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
 
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
 
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
