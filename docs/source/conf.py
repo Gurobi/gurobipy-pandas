fb7701261b6a468233ab88051bb0a04ce12a23fb
Simon Bowly <bowly@gurobi.com>
2022-08-02 14:15:35 +1000

    Remove rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,10 +42,6 @@ intersphinx_disabled_domains = ["std"]
 
 templates_path = ["_templates"]
 
-# -- Options for HTML output -------------------------------------------------
-
-html_theme = "sphinx_rtd_theme"
-
 # -- Options for EPUB output
 
 epub_show_urls = "footnote"
