003f1bb747dbe6c487799c870e2d18b329bce8c7
Simon Bowly <bowly@gurobi.com>
2022-08-02 13:43:55 +1000

    ReST docstrings for index and series accessors

--- docs/source/api.rst
+++ docs/source/api.rst
@@ -3,3 +3,9 @@ API
 
 .. autoclass:: gurobipy_pandas.accessors.GRBDataFrameAccessor
     :members: pd_add_vars, pd_add_constrs
+
+.. autoclass:: gurobipy_pandas.accessors.GRBSeriesAccessor
+    :members: __getattr__, __setattr__, get_value
+
+.. autoclass:: gurobipy_pandas.accessors.GRBIndexAccessor
+    :members: pd_add_vars
--- src/gurobipy_pandas/accessors.py
+++ src/gurobipy_pandas/accessors.py
@@ -9,6 +9,15 @@ from gurobipy import GRB
 
 @pd.api.extensions.register_dataframe_accessor("grb")
 class GRBDataFrameAccessor:
+    """Accessor class for methods invoked as :code:`pd.DataFrame(...).grb.*`.
+    The accessor does not expect particular types in the dataframe. This
+    class should not be instantiated directly; it should be used via Pandas'
+    accessor API
+
+    :param pandas_obj: A pandas dataframe
+    :type pandas_obj: :class:`pd.DataFrame`
+    """
+
     def __init__(self, pandas_obj: pd.DataFrame):
         self._obj = pandas_obj
 
@@ -206,12 +215,45 @@ class GRBDataFrameAccessor:
 
 @pd.api.extensions.register_series_accessor("grb")
 class GRBSeriesAccessor:
+    """Accessor class for methods invoked as :code:`pd.Series(...).grb.*`. The
+    accessor expects a series containing gurobipy objects, and can return new
+    series by evaluating a target value across all objects in the series. This
+    class should not be instantiated directly; it should be used via Pandas'
+    accessor API
+
+    :param pandas_obj: A pandas series
+    :type pandas_obj: :class:`pd.Series`
+    """
+
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
     def __getattr__(self, attr):
-        """Use Gurobi getAttr (not builtin) to access Var.X, Var.lb,
-        Constr.Slack , etc attribute values series-wise."""
+        """Retrieve the given Gurobi attribute for every object in the
+        Series held by this accessor
+
+        :return: A series with the evaluated attributes
+        :rtype: :class:`pd.Series`
+
+        For example, after solving a model, solution values can be read
+        using the :code:`X` attribute.
+
+        Build and solve a little model:
+
+        >>> m = gp.Model()
+        >>> df = pd.DataFrame({"key": [1, 2, 2, 2], "obj": [4, 3, 2, 1]})
+        >>> df = df.grb.pd_add_vars(m, name="x", vtype=gp.GRB.BINARY, obj="obj")
+        >>> grouped = df.groupby('key')[['x']].sum().grb.pd_add_constrs(m, "x == 1", name="constr")
+        >>> m.optimize()  # doctest: +ELLIPSIS
+        Gurobi Optimizer version...
+        Best objective 5.000000000000e+00, best bound 5.000000000000e+00, gap 0.0000%
+        >>> df['x'].grb.X
+        0    1.0
+        1    0.0
+        2    0.0
+        3    1.0
+        Name: x, dtype: float64
+        """
         return pd.Series(
             index=self._obj.index,
             data=[v.getAttr(attr) for v in self._obj],
@@ -219,6 +261,42 @@ class GRBSeriesAccessor:
         )
 
     def __setattr__(self, attr, value):
+        """Implements Python built-in :code:`__setattr__` to change the
+        given Gurobi attribute for every object in the Series held by this
+        accessor
+
+        :return: A new series with the evaluated attributes
+        :rtype: :class:`pd.Series`
+
+        For example, after creating a series of variables, their upper
+        bounds can be set and retrieved.
+
+        >>> m = gp.Model()
+        >>> x = pd.RangeIndex(3).grb.pd_add_vars(m)
+        >>> m.update()
+        >>> x
+        0    <gurobi.Var C0>
+        1    <gurobi.Var C1>
+        2    <gurobi.Var C2>
+        dtype: object
+        >>> upper_bounds = pd.Series(data=[3, 2], index=[1, 2])
+        >>> upper_bounds
+        1    3
+        2    2
+        dtype: int64
+        >>> x.grb.UB = upper_bounds
+
+        After setting upper bounds (by aligning the variable series :code:`x`
+        with the data series :code:`upper_bounds`), attributes can be read
+        back from the Gurobi objects.
+
+        >>> m.update()
+        >>> x.grb.UB
+        0    inf
+        1    3.0
+        2    2.0
+        dtype: float64
+        """
         if attr == "_obj":
             super().__setattr__(attr, value)
         elif isinstance(value, pd.Series):
@@ -232,6 +310,14 @@ class GRBSeriesAccessor:
                 v.setAttr(attr, value)
 
     def get_value(self):
+        """Return a new series, on the same index, containing the result of
+        :code:`obj.getValue()` for each gurobipy object in the series held
+        by this accessor. Note that this assumes that the wrapped objects are
+        gurobipy expressions (:class:`LinExpr` or :class:`QuadExpr`)
+
+        :return: A series with the evaluated expression values
+        :rtype: :class:`pd.Series`
+        """
         return pd.Series(
             index=self._obj.index,
             data=[le.getValue() for le in self._obj],
@@ -240,13 +326,47 @@ class GRBSeriesAccessor:
 
 @pd.api.extensions.register_index_accessor("grb")
 class GRBIndexAccessor:
+    """Accessor class for methods invoked as :code:`pd.Index(...).grb.*`. The
+    accessor expects normal pandas types in the index, and can return new
+    series of gurobipy objects aligned to that index. This class should not be
+    instantiated directly; it should be used via Pandas' accessor API
+
+    :param pandas_obj: A pandas index
+    :type pandas_obj: :class:`pd.Index`
+    """
+
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
     def pd_add_vars(
-        self, model, lb=0.0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS, name=None
+        self,
+        model: gp.Model,
+        lb: float = 0.0,
+        ub: float = GRB.INFINITY,
+        vtype: str = GRB.CONTINUOUS,
+        name: Optional[str] = None,
     ):
-        """Given an index, return a series of Vars with that index."""
+        """Add a variable to the given model for each entry in the index
+        referenced by this accessor.
+
+        :param model: A Gurobi model to which new variables will be added
+        :type model: :class:`gurobipy.Model`
+        :param lb: Lower bound for created variables, defaults to 0.0
+        :type lb: float, optional
+        :param ub: Upper bound for created variables, defaults to
+            :code:`GRB.INFINITY`
+        :type ub: float, optional
+        :param obj: Objective function coefficient for created variables,
+            defaults to 0.0
+        :type obj: float, optional
+        :param vtype: Gurobi variable type for created variables, defaults
+            to :code:`GRB.CONTINUOUS`
+        :type vtype: str, optional
+        :param name: If provided, used as base name for new Gurobi variables
+        :type name: str, optional
+        :return: A Series of vars with the index referenced by the accessor
+        :rtype: :class:`pd.Series`
+        """
         if name is None:
             indices = len(self._obj)
         else:
