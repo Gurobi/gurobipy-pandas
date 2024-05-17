ab035d8193d1df066bb7829e78caceec745ee677
Irv Lustig <irv@princeton.com>
2024-05-17 17:03:38 -0400

    fix init to export methods

--- src/gurobipy_pandas/__init__.py
+++ src/gurobipy_pandas/__init__.py
@@ -3,6 +3,6 @@ __version__ = "1.1.1"
 # Import public API functions
 # Import accessors module to register accessors.
 import gurobipy_pandas.accessors  # noqa: F401
-from gurobipy_pandas.api import add_constrs  # noqa: F401
-from gurobipy_pandas.api import add_vars  # noqa: F401
-from gurobipy_pandas.api import set_interactive  # noqa: F401
+from gurobipy_pandas.api import add_constrs as add_constrs  # noqa: F401
+from gurobipy_pandas.api import add_vars as add_vars # noqa: F401
+from gurobipy_pandas.api import set_interactive as set_interactive  # noqa: F401
