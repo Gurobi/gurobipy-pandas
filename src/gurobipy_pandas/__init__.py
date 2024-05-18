af50372b1f667f0bfb973f840d5094b89d6ed882
Simon Bowly <bowly@gurobi.com>
2024-05-18 10:54:05 -0400

    Define public API in __all__

--- src/gurobipy_pandas/__init__.py
+++ src/gurobipy_pandas/__init__.py
@@ -1,8 +1,9 @@
 __version__ = "1.1.1"
 
-# Import public API functions
-# Import accessors module to register accessors.
+__all__ = ["add_constrs", "add_vars", "set_interactive"]
+
+# Import accessors module to register accessors with pandas
 import gurobipy_pandas.accessors  # noqa: F401
-from gurobipy_pandas.api import add_constrs  # noqa: F401
-from gurobipy_pandas.api import add_vars  # noqa: F401
-from gurobipy_pandas.api import set_interactive  # noqa: F401
+
+# Import public API functions
+from gurobipy_pandas.api import add_constrs, add_vars, set_interactive
