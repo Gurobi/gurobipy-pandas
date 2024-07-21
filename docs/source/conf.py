ed66e646feef9a7667d77ca087bce773be31913e
Simon Bowly <bowly@gurobi.com>
2024-07-21 23:46:38 +1000

    Add example notebooks download banner

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,3 +83,14 @@ numpydoc_class_members_toctree = False
 # -- Options for EPUB output
 
 epub_show_urls = "footnote"
+
+# -- Note pointing to notebook downloads
+
+nbsphinx_prolog = """
+
+.. note::
+
+   This is example is available as a Jupyter notebook. Download it and all
+   necessary data files :download:`here </gurobipy-pandas-examples.zip>`.
+
+"""
